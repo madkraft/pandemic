5fc762ce88aa7593765014d9fae58210b275dd6f
antonkostiuchkov <antonkostiuchkov@gmail.com>
2017-02-12 00:57:41 +0100

    added card deck

--- src/app/game-board/containers/board/board.component.ts
+++ src/app/game-board/containers/board/board.component.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'board',
   template: `
-    <card-deck></card-deck>
+    <div>
+      <h1>Infection Deck</h1>
+      <card-deck [cards]="infectionCards"></card-deck>
+
+      <h1>Play Deck</h1>
+      <card-deck [cards]="playCards"></card-deck>
+    </div>
   `,
   styleUrls: ['./board.component.scss']
 })
 
-export class BoardComponent {
+export class BoardComponent implements OnInit {
+  infectionCards;
+  playCards;
 
+  ngOnInit () {
+    this.infectionCards = [
+      {
+        city: 'Bangkok',
+        color: '#c01025'
+      },
+      {
+        city: 'Mexico',
+        color: '#fed024'
+      },
+      {
+        city: 'Mumbai',
+        color: '#1e2a2d'
+      },
+      {
+        city: 'Atlanta',
+        color: '#1007ed'
+      },
+      {
+        city: 'Osaka',
+        color: '#c01025'
+      }
+    ];
+
+    this.playCards = [
+      {
+        city: 'Shanghai',
+        color: '#c01025'
+      },
+      {
+        city: 'Buenos Aires',
+        color: '#fed024'
+      },
+      {
+        city: 'Moscow',
+        color: '#1e2a2d'
+      },
+      {
+        city: 'New York',
+        color: '#1007ed'
+      },
+      {
+        city: 'Seul',
+        color: '#c01025'
+      }
+    ];
+  }
 }
